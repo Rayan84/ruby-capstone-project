7901408f75f4d9626e22af22ab3923202ba08c92
Rayan84 <rayan_84@live.com>
2022-01-21 00:07:36 +0100

    Add class item attributes

--- item.rb
+++ item.rb
@@ -8,4 +8,18 @@ class Item
     @publish_date = date
     @archived = false
   end
+
+  # one to many should not be set in constructor, instead they should have dependent setter methods
+
+  def can_be_archived?(item)
+    if item.publish_date > 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def move_to_archived(item)
+    return item.archived = true if can_be_archived?(item)
+  end
 end
